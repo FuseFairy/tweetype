d456140fe0375342c99dcf85b9b04176b3ab3f5a
FuseFairy <dd8611706@gmail.com>
2024-12-09 20:14:54 +0800

    fix: invalid package

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+install_requires = [
+    "beautifulsoup4[lxml]~=4.12",
+    "openpyxl",
+    "httpx[http2]",
+    "dateutils",
+    "anticaptchaofficial",
+    "capsolver",
+    "2captcha-python",
+    "filetype"
+]
 
 setup(
-    name="tweety-ns",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "beautifulsoup4[lxml]>=4.12,<5.0",
-        "openpyxl",
-        "httpx[http2]",
-        "python-dateutil",
-        "anticaptchaofficial",
-        "capsolver",
-        "2captcha-python",
-        "filetype"
+    name='tweety-ns',
+    packages=['tweety', 'tweety.types', 'tweety.events', 'tweety.captcha'],
+    version='2.0.7',
+    license='MIT',
+    description='An easy Twitter Scraper',
+    author='Tayyab Kharl',
+    author_email='tayyabmahr@gmail.com',
+    url='https://github.com/mahrtayyab/tweety',
+    keywords=['TWITTER', 'TWITTER SCRAPE', 'SCRAPE TWEETS'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
     ],
 )
